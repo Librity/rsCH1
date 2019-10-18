093372248fa1c70a1d19623118bfd5507ee892ed
Librity <luis.geniole@gmail.com>
2019-10-17 22:17:07 -0300

    revise, refactor & optimize

--- index.js
+++ index.js
@@ -5,23 +5,9 @@ const server = express();
 server.use(express.json());
 
 let request_count = 0;
+const projects = [];
 
-let projects = [
-  {
-    id: "3",
-    title: "dsad",
-    tasks: []
-  }
-];
-
-server.use((req, res, next) => {
-  request_count++;
-  console.log(`${request_count} requests so far...`);
-
-  return next();
-});
-
-function findProjectById(req, res, next) {
+findProjectById = (req, res, next) => {
   if (req.body.id) {
     res.locals.req_id = req.body.id;
   } else if (req.params.id) {
@@ -30,30 +16,36 @@ function findProjectById(req, res, next) {
     return res.status(400).json({ error: "project id required" });
   }
 
-  res.locals.project = projects.filter(project => {
-    return project.id.indexOf(res.locals.req_id) > -1;
-  });
+  res.locals.project = projects.find(p => p.id == res.locals.req_id);
 
   return next();
-}
+};
 
-function checkProjectIdTaken(req, res, next) {
-  if (res.locals.project.length != 0) {
+checkProjectIdTaken = (req, res, next) => {
+  if (res.locals.project) {
     return res
       .status(400)
       .json({ error: "id already used by another project" });
   }
 
   return next();
-}
+};
 
 function checkProjectIdValid(req, res, next) {
-  if (res.locals.project.length == 0) {
+  if (!res.locals.project) {
     return res.status(400).json({ error: "invalid id: project doesn't exist" });
   }
 
   return next();
 }
+
+server.use((req, res, next) => {
+  request_count++;
+
+  console.log(`${request_count} requests so far...`);
+
+  return next();
+});
 
 server.post("/projects", findProjectById, checkProjectIdTaken, (req, res) => {
   const { title } = req.body;
@@ -76,23 +68,7 @@ server.put(
   (req, res) => {
     const new_title = req.body.title;
 
-    res.locals.project[0].title = new_title;
-
-    /***************************************************************************
-     * We either call checkProjectIdTaken or we update the title of every
-     * project with that id (not a good idea);
-     ***************************************************************************
-     *var searchTerm = id;
-     *projects
-     *  .filter(project => {
-     *    return project.id.indexOf(searchTerm) > -1;
-     *  })
-     *  .forEach(changeTitleARRAY);
-     *
-     *function changeTitleARRAY(element) {
-     *  element.title = new_title;
-     *}
-     **************************************************************************/
+    res.locals.project.title = new_title;
 
     return res.json(res.locals.project);
   }
@@ -120,7 +96,7 @@ server.post(
   (req, res) => {
     const { title } = req.body;
 
-    res.locals.project[0].tasks.push(title);
+    res.locals.project.tasks.push(title);
 
     return res.json(res.locals.project);
   }
